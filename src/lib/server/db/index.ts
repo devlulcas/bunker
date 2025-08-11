3c740fda0b99d54590215741da69d79f793b0661
devlulcas <lucasalvesrego.contato@gmail.com>
2025-08-11 14:02:51 -0300

    chore: update .env.example with new authentication tokens and project URL

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,11 +1,7 @@
-import { dev } from '$app/environment';
 import { env } from '$env/dynamic/private';
 import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
 import * as schema from './schema';
-
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-if (!dev && !env.DATABASE_AUTH_TOKEN) throw new Error('DATABASE_AUTH_TOKEN is not set');
 
 const client = createClient({
 	url: env.DATABASE_URL,
