763ed188c9e2a9b91cdc1af4fb9436677b89c2fe
devlulcas <lucasalvesrego.contato@gmail.com>
2025-08-11 13:52:30 -0300

    feat: enhance layout metadata and disallow web crawling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
-	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import '../app.css';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>Fusca</title>
+	<meta name="description" content="there might be dragons" />
+	<meta name="robots" content="noindex" />
+	<meta name="author" content="Lucas Alves Rego" />
+	<meta name="theme-color" content="#000000" />
 </svelte:head>
 
 {@render children?.()}
